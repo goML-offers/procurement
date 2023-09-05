945f0f4516ee53ff223b1ca5a836eb80ad7a2962
KisaZehra-goML <137411918+KisaZehra-goML@users.noreply.github.com>
2023-09-05 18:02:42 +0530

    Update s3_data.py

--- api/services/s3_data.py
+++ api/services/s3_data.py
@@ -5,13 +5,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-os.environ['AWS_ACCESS_KEY_ID'] = os.environ.get("AWS_ACCESS_KEY_ID_S3")
-os.environ['AWS_SECRET_ACCESS_KEY'] = os.environ.get("AWS_SECRET_ACCESS_KEY_S3")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID_S3")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY_S3")
 
 
 
-session = Session(aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+session = Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
+aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 s3 = session.resource('s3')
 s3_client = s3.Bucket('gomloffers')
 
